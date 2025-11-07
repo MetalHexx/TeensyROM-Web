004b1072d9469831234b185ce4b3da356ec5664b
metalhexx <metalhexx@outlook.com>
2025-11-07 00:49:08 -0500

    chore: moved everything into /src and removed /api and /ui folders.

--- ui/apps/api/TeensyRom.Core.Midi/MidiEvent.cs
+++
@@ -1,6 +0,0 @@
-﻿using TeensyRom.Core.Entities.Midi;
-
-namespace TeensyRom.Core.Midi
-{
-    public record MidiEvent(DJEventType DJEventType, MidiEventType MidiEventType, MidiMapping Mapping, int Value);
-}
